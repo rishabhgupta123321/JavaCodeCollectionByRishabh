198c796ad85e819a9fdcd93b016b3ef9d4f2ad53
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-06-17 14:13:32 +0530

    Update Fullexplanation.java

--- Fullexplanation.java
+++ Fullexplanation.java
@@ -1,55 +1,17 @@
-// public class Fullexplanation {
-//     public static void main(String[] a){
-//         System.out.print("Hello this is full explanation of public static void main");
-//     }
-// }
+public class Fullexplanation {
+    public static void main(String[] a){
+        System.out.print("Hello this is full explanation of public static void main");
+    }
+}
 
 
 
 
-// public class Fullexplanation {
-//     static void main(String[] a){
-//         System.out.print("Hello this is full explanation of public static void main");
-//     }
-// }
-
-
-
-
-
-
-
-// public class Fullexplanation {
-//     public void main(String[] a){
-//         System.out.print("Hello this is full explanation of public static void main");
-//     }
-// }
-
-
-
-
-
-// public class Fullexplanation {
-//     void make(){
-//         System.out.println("make this");
-//     }
-//     public static void main(String[] a){
-//         make();
-//         System.out.print("Hello this");
-//     }
-// }
-
-
-
-// public class Fullexplanation {
-//     void make(){
-//         System.out.println("make this");
-//     }
-//     public static void main(String[] a){
-//         Fullexplanation.make();
-//         System.out.print("Hello this");
-//     }
-// }
+public class Fullexplanation {
+    static void main(String[] a){
+        System.out.print("Hello this is full explanation of public static void main");
+    }
+}
 
 
 
@@ -57,16 +19,37 @@
 
 
 
-// public class Fullexplanation {
-//     void make(){
-//         System.out.println("make this");
-//     }
-//     public static void main(String[] a){
-//         Fullexplanation ref = new Fullexplanation();
-//         ref.make();
-//         System.out.print("Hello this");
-//     }                                  
-// }
+public class Fullexplanation {
+    public void main(String[] a){
+        System.out.print("Hello this is full explanation of public static void main");
+    }
+}
+
+
+
+
+
+public class Fullexplanation {
+    void make(){
+        System.out.println("make this");
+    }
+    public static void main(String[] a){
+        make();
+        System.out.print("Hello this");
+    }
+}
+
+
+
+public class Fullexplanation {
+    void make(){
+        System.out.println("make this");
+    }
+    public static void main(String[] a){
+        Fullexplanation.make();
+        System.out.print("Hello this");
+    }
+}
 
 
 
@@ -74,20 +57,16 @@
 
 
 
-
-
-
-
-
-// public class Fullexplanation {
-//     static void make(){
-//         System.out.println("make this");
-//     }
-//     public static void main(String[] a){
-//         make();
-//         System.out.print("Hello this");
-//     }                                  
-// }
+public class Fullexplanation {
+    void make(){
+        System.out.println("make this");
+    }
+    public static void main(String[] a){
+        Fullexplanation ref = new Fullexplanation();
+        ref.make();
+        System.out.print("Hello this");
+    }                                  
+}
 
 
 
@@ -98,15 +77,17 @@
 
 
 
-// public class Fullexplanation {
-//     static void make(){
-//         System.out.println("make this");
-//     }
-//     public static void main(String[] a){
-//         Fullexplanation.make();
-//         System.out.print("Hello this");
-//     }                                  
-// }
+
+
+public class Fullexplanation {
+    static void make(){
+        System.out.println("make this");
+    }
+    public static void main(String[] a){
+        make();
+        System.out.print("Hello this");
+    }                                  
+}
 
 
 
@@ -116,22 +97,41 @@
 
 
 
-// public class Fullexplanation {
-//     public static void main(String[] arg){
-//         System.out.print(arg[1]);
-//     }                                  
-// }
+
+public class Fullexplanation {
+    static void make(){
+        System.out.println("make this");
+    }
+    public static void main(String[] a){
+        Fullexplanation.make();
+        System.out.print("Hello this");
+    }                                  
+}
 
 
 
 
 
 
-// public class Fullexplanation {
-//     public static void main(String[] abd){
-//         System.out.print(abd[2]);
-//     }                                  
-// }
+
+
+
+public class Fullexplanation {
+    public static void main(String[] arg){
+        System.out.print(arg[1]);
+    }                                  
+}
+
+
+
+
+
+
+public class Fullexplanation {
+    public static void main(String[] abd){
+        System.out.print(abd[2]);
+    }                                  
+}
 
 
 
