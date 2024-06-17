35a1cf2a0ea6da7ea6055f3e438c2870cfb9285a
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-06-17 14:12:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,27 @@
-/**
- * Main
- */    // FOR RUNNING THE JAVA PROGRAM I HAVE THE SHORTCUT COMMAND WHICH IS CTRL+'
 
 
-//  public class Main {
+ public class Main {
 
-//     public static void main(String[] args) {
-//         System.out.println("HelloO");
-//     }
-// }
+    public static void main(String[] args) {
+        System.out.println("HelloO");
+    }
+}
 
 
 
 
 
 
-/**
- * Main
- */
-
-
-/* HellklkhjjjjO
-hello 
- */
 
 
 
-// public class Main {
-//     public static void main(String[] args) {
-//         System.out.println("comments has two methods");
-//     }
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("comments has two methods");
+    }
     
-// }
+}
 
 
 
