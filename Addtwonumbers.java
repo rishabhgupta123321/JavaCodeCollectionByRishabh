1ce2811ac5312c1620d99302ee04969b53c7b8ef
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-06-17 22:47:49 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,9 @@
+     /* Add Two Numbers Program in java(Without user input)  */ 
+
+
+class Addtwonumbers{
+    public static void main(String args[]){
+        int a = 2, b = 4;   
+        System.out.print(a+b);
+    }
+}
