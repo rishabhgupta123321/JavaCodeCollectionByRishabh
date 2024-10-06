76900b0007e863d50fa4d6b84988f8ade91638e4
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-10-06 18:03:53 +0530

    Update Alphabets.java

--- Alphabets.java
+++ Alphabets.java
@@ -1,63 +1,58 @@
-              /*print A to Z alphabets- */
+/* Print A to Z alphabets using a for loop. */
 
-
-
-
-/**
-class Alphabets{
+class PrintAlphabetsAtoZ {
     public static void main(String[] args) {
-        for(char i = 'A'; i<='Z' ; i++) {     // when we write character then we write this with single qoute.           
-            System.out.print(i+" ");
-        }
-    } 
-}
-
-*/
-
-
-    // in this after z when this loop will go to i++ then in this because 
-    // z ASCII value is 90 so when i++ happen then this will become 91 
-    //and 91 ASCII value is for      [    this character.           '
-  // it means in this we know that in the left square bracket    '['    is greater than 'Z'.
-
-
-/**
-class Alphabets{
-    public static void main(String[] args) {
-        for(char ch = 'A'; ch<='Z'; ch++){
-            System.out.print(ch+" ");
+        for (char i = 'A'; i <= 'Z'; i++) { // Character should be enclosed in single quotes.
+            System.out.print(i + " ");
         }
     }
 }
-*/
 
+/* 
+ * Explanation:
+ * After 'Z', the loop increments 'i', which becomes 91 (ASCII value).
+ * 91 corresponds to the character '[' which is greater than 'Z'.
+ */
 
+class PrintAlphabetsAtoZAlternative {
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
 
-
-
-// Requirement 
-// think about logic and write basic syntax and then this will be done.
+/* 
+ * Requirement: 
+ * Write a program that prints letters from 'A' to a given character. 
+ * Logic: Take input from the user and print characters up to the input character.
+ */
 
 import java.util.Scanner;
-class Alphabets{
-    public static void main(String[] args) {
-        Scanner r = new Scanner(System.in);
-        char cha;
-        System.out.print("Enter any character: ");
-        cha = r.nextLine().charAt(0);
-        if(cha>='a' && cha<='z'){
-        for(char ch = 'a'; ch<=cha; ch++){ // in this we can use also while and do-while loop but in mostly we use for loop that's why we have taken this loop.
-            System.out.print(ch+" ");
-        }
-    }
-    if(cha>='A' && cha<='Z'){
-        for(char ch = 'A'; ch<=cha; ch++){
-            System.out.print(ch+" ");
-        }
-    }
 
-        r.close();
+class PrintAlphabetsUpToInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char inputChar;
+
+        System.out.print("Enter any character: ");
+        inputChar = scanner.nextLine().charAt(0);
+
+        // Check if the input character is lowercase.
+        if (inputChar >= 'a' && inputChar <= 'z') {
+            for (char ch = 'a'; ch <= inputChar; ch++) { 
+                // You can also use while or do-while loop, but for loop is commonly used here.
+                System.out.print(ch + " ");
+            }
+        }
+
+        // Check if the input character is uppercase.
+        if (inputChar >= 'A' && inputChar <= 'Z') {
+            for (char ch = 'A'; ch <= inputChar; ch++) {
+                System.out.print(ch + " ");
+            }
+        }
+
+        scanner.close();
     }
 }
-
-    
