1170cfe50b0f2d70dc4d292bf1f08125b5961adf
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-10-06 20:28:26 +0530

    Create InvertedPyramidPattern.java

---
+++ InvertedPyramidPattern.java
@@ -0,0 +1,14 @@
+class InvertedPyramidPattern {
+    public static void main(String[] args) {
+        int i, j, k;
+        for (i = 1; i <= 5; i++) {  // rows
+            for (k = 5; k > i; k--) {  // spaces
+                System.out.print("  ");
+            }
+            for (j = 1; j <= (i + (i - 1)); j++) {  // columns
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
