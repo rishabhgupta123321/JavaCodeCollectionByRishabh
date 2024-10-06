3ca4b5788f4b7bb614f95f9e77857d72aa25a687
Rishabh gupta <115399781+rishabhgupta123321@users.noreply.github.com>
2024-10-06 16:23:58 +0530

    HelloWorldExample.java

--- lecture_6_comp_excu.java
+++ lecture_6_comp_excu.java
@@ -1,5 +1,7 @@
-class Lecture_6_comp_excu{
-    public static void main(String args[]){
+// This is a simple Java program that prints "Hello World!" to the console.
+public class HelloWorldExample {
+    public static void main(String[] args) {
+        // Output the message to the console
         System.out.print("Hello World!");
     }
 }
